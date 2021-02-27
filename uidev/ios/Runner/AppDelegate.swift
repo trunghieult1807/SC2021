b32cf1c251e948a9878b3efa3025f7a7a1e81579
Thanh Nguyen <tienthanhnt123@gmail.com>
2021-02-27 22:28:48 +0700

    Configure openning screen when notification up

--- uidev/ios/Runner/AppDelegate.swift
+++ uidev/ios/Runner/AppDelegate.swift
@@ -6,6 +6,9 @@ import Flutter
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
